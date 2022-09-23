8fe517bb1e2d68d5df8a5663b5e319321faea432
cristiano-sales <cristianosalesneves@gmail.com>
2022-09-23 19:35:28 -0300

    test: req 11

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,20 @@
-# from src.brazilian_jobs import read_brazilian_file
+from src.brazilian_jobs import read_brazilian_file
 
 
 def test_brazilian_jobs():
-    pass
+    test_brazilian_files = read_brazilian_file("tests/mocks/brazilians_jobs.csv")
+    assert test_brazilian_files[0] == {
+        "title": "Maquinista",
+        "salary": "2000",
+        "type": "trainee"
+    }
+    assert test_brazilian_files[5] == {
+        "title": "Auxiliar usinagem",
+        "salary": "1400",
+        "type": " full time"
+    }
+    assert test_brazilian_files[10] == {
+        "title": "Esportista de Futebol profissional",
+        "salary": "50000",
+        "type": " full time"
+    }
