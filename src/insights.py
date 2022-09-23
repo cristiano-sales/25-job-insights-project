c4edd35ae141ef9fec68f5788a5471356ff72553
cristiano-sales <cristianosalesneves@gmail.com>
2022-09-23 18:48:29 -0300

    feat: req 08

--- src/insights.py
+++ src/insights.py
@@ -1,3 +1,4 @@
+from operator import is_
 from src.jobs import read
 
 
@@ -40,21 +41,15 @@ def get_unique_industries(path):
 
 
 def filter_by_industry(jobs, industry):
-    """Filters a list of jobs by industry
 
-    Parameters
-    ----------
-    jobs : list
-        List of jobs to be filtered
-    industry : str
-        Industry for the list filter
+    filtered_industries = []
 
-    Returns
-    -------
-    list
-        List of jobs with provided industry
-    """
-    return []
+    for j in jobs:
+
+        if industry == j["industry"]:
+            filtered_industries.append(j)
+
+    return filtered_industries
 
 
 def get_max_salary(path):
@@ -88,29 +83,23 @@ def get_min_salary(path):
 
 
 def matches_salary_range(job, salary):
-    """Checks if a given salary is in the salary range of a given job
+    def is_int(value):
+        return type(value) == int
 
-    Parameters
-    ----------
-    job : dict
-        The job with `min_salary` and `max_salary` keys
-    salary : int
-        The salary to check if matches with salary range of the job
+    if (
+        "min_salary" not in job
+        or "max_salary" not in job
+        or not is_int(job["min_salary"])
+        or not is_int(job["max_salary"])
+        or not is_int(salary)
+        or job["min_salary"] > job["max_salary"]
+    ):
+        raise ValueError("Invalid values")
 
-    Returns
-    -------
-    bool
-        True if the salary is in the salary range of the job, False otherwise
+    elif job["min_salary"] <= salary <= job["max_salary"]:
+        return True
 
-    Raises
-    ------
-    ValueError
-        If `job["min_salary"]` or `job["max_salary"]` doesn't exists
-        If `job["min_salary"]` or `job["max_salary"]` aren't valid integers
-        If `job["min_salary"]` is greather than `job["max_salary"]`
-        If `salary` isn't a valid integer
-    """
-    pass
+    return False
 
 
 def filter_by_salary_range(jobs, salary):
